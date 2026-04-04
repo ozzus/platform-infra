ce302a3485385ff67e829c20746521e2a5d1168c
Ozzus <serj.reintov228@gmail.com>
2026-04-04 15:05:51 +0300

    chore: add local development stack

---
+++ local/postgres/init/01-databases.sql
@@ -0,0 +1,8 @@
+create user registry with password 'registry';
+create database diasoft_registry owner registry;
+
+create user gateway with password 'gateway';
+create database diasoft_gateway owner gateway;
+
+create user keycloak with password 'keycloak';
+create database diasoft_keycloak owner keycloak;
